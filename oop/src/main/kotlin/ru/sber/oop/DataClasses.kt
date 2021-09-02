f6d1ac52b5ee1d8cd6f26133c92ec4aa52b4c8e8
Исламбек Туранов <islambek198@gmail.com>
2021-09-02 12:50:42 +0300

    "DataClasses" done

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -1,11 +1,31 @@
 package ru.sber.oop
 
+import java.util.Objects
+
 data class User(val name: String, val age: Long) {
     lateinit var city: String
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is User) return false
+        return try {
+            other.name == name && other.age == age && other.city == city
+        } catch (e: UninitializedPropertyAccessException) {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(name.hashCode(), age.hashCode(), city.hashCode())
+    }
 }
 
 fun main() {
     val user1 = User("Alex", 13)
-    //TODO: user2 = ...
-    //TODO: user3 = ...
+    val user2 = user1.copy(name = "ander")
+    user1.city = "Omsk"
+    val user3 = user1.copy()
+    user3.city = "Tomsk"
+    println(user1.equals(user3))
+    println(user1.equals(user2))
+    println(user2.equals(user3))
 }
