2bf1f0198d6ce6222a462b45b29d0a25eee47a18
Исламбек Туранов <islambek198@gmail.com>
2021-09-02 15:47:36 +0300

    "Polymorphism" done and some changes in "Inheritance"

--- oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
+++ oop/src/main/kotlin/ru/sber/oop/Inheritance.kt
@@ -1,19 +1,70 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 open class Room(var name: String, var size: Int) {
 
     protected open val dangerLevel = 5
+    val monster: Monster = Goblin(
+        "Abaddon",
+        "Abaddon, the Lord of Avernus. \tThe Font of Avernus is the source of a family's strength," +
+                " a crack in primal stones from which vapors of prophetic power have issued for generations. " +
+                "Each newborn of the cavernous House Avernus is bathed in the black mist, and by this baptism t" +
+                "hey are given an innate connection to the mystic energies of the land. They grow up believing" +
+                " themselves fierce protectors of their lineal traditions, the customs of the realm—but what they " +
+                "really are protecting is the Font itself. And the motives of the mist are unclear.",
+        250,
+        "Wizard"
+    )
 
     constructor(name: String) : this(name, 100) {
     }
 
     open fun description() = "Room: $name"
 
-    open fun load() = "Nothing much to see here..."
+    open fun load() {
+        println("Welcome to \"$name\"")
+        monster.getSalutation()
+    }
 
 }
 
 class TownSquare : Room("Town Square", 1000) {
-    final override fun load() = "Loading..."
+    final override fun load() {
+        println("Welcome to $name")
+    }
+
     override val dangerLevel = super.dangerLevel - 3
+}
+
+// this is out of homework
+
+fun startGame(room: Room, player: Player) {
+    val monster = room.monster
+    room.load()
+    while (monster.healthPoints > 0 && player.healthPoints > 0) {
+        val chooseAttacker = Random(System.nanoTime()).nextInt(0, 2)
+        if (chooseAttacker == 1)
+            println("${player.name} attacked to ${monster.name}, damage roll = ${player.attack(monster)}")
+        else
+            println("${monster.name} attacked to ${player.name}, damage roll = ${monster.attack(player)}")
+        if (player.healthPoints < 0)
+            player.healthPoints = 0
+        if (monster.healthPoints < 0)
+            monster.healthPoints = 0
+        println("${player.name}: health = ${player.healthPoints}")
+        println("${monster.name}: health = ${monster.healthPoints}")
+        println("---------------------------------------------")
+        if (monster.healthPoints == 0)
+            println("${monster.name} is dead, win ${player.name}")
+        if (player.healthPoints == 0)
+            println("${player.name} is dead, win ${monster.name}")
+        Thread.sleep(2000)
+    }
+}
+
+fun main() {
+    val room = Room("Meeting with a monster")
+    val player = Player("Kunkka", false, 200, "Admiral")
+    startGame(room, player)
 }
--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -1,13 +1,46 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = Random(System.nanoTime()).nextInt(0, 50)
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player(
+    val name: String,
+    val isBlessed: Boolean,
+    override var healthPoints: Int,
+    override val powerType: String,
+) : Fightable {
 
+    override fun attack(opponent: Fightable): Int {
+        val amountDamageRoll: Int = if (isBlessed) damageRoll * 2 else damageRoll
+        opponent.healthPoints -= amountDamageRoll
+        return amountDamageRoll
+    }
+}
 
+abstract class Monster(val name: String, val description: String) : Fightable {
+
+    override fun attack(opponent: Fightable): Int {
+        opponent.healthPoints -= damageRoll
+        return damageRoll
+    }
+}
+
+class Goblin(
+    _name: String,
+    _description: String,
+    override var healthPoints: Int,
+    override val powerType: String,
+) : Monster(_name, _description) {
+    override val damageRoll: Int
+        get() = super.damageRoll / 2
+}
+
+fun Monster.getSalutation() = println("$name: UaUaAaAa!!! You will die today!")
